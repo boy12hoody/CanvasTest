a200666c875d88834ebed6d7c48c05e73c3eae1c
boy12hoody <boy12hoody@gmail.com>
2021-10-11 06:13:18 -0700

    Saves generated state for animations

--- app/src/main/java/uz/boywonder/canvastest/util/CustomView.kt
+++ app/src/main/java/uz/boywonder/canvastest/util/CustomView.kt
@@ -27,14 +27,44 @@ class CustomView @JvmOverloads constructor(
 
     private val paint by lazy { Paint(Paint.ANTI_ALIAS_FLAG) }
     private var chosenEventType = 0
+
+    /* Restricts onDraw from generating random white-black area again and again */
     private var toDrawGrid = false
+
+    /* Avoids initial blank area being clicked */
     private var hasGenerated = false
 
+    /* List to hold random generated area rectangles to use with animations */
+    private val rectFloatList = mutableListOf<RectF>()
+
+    private val sizeRect = RectF(0f, 0f, 0f, 0f)
+
+    private var startX = 0f
+    private var startY = 0f
+    private var stopX = 0f
+    private var stopY = 0f
+
+    /* Exposed function to draw/generate white-black area */
     fun drawGrid() {
         toDrawGrid = true
         animator?.cancel()
         invalidate()
 
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        sizeRect.set(0f, 0f, w.toFloat(), h.toFloat())
+        Log.d("CANVAS", "onSizeChanged called")
+
+        setSizeRect()
+    }
+
+    private fun setSizeRect() {
+        startX = 0f
+        startY = sizeRect.height()
+        stopX = sizeRect.width()
+        stopY = sizeRect.height()
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -94,22 +124,36 @@ class CustomView @JvmOverloads constructor(
         val maxCanvasValue = maxOf(canvas.width, canvas.height)
         val grid = maxCanvasValue / 50
 
+        rectFloatList.clear()
+        setSizeRect()
+
         for (i in 0..maxCanvasValue step grid) {
 
             for (j in 0..maxCanvasValue step grid) {
 
                 if (Random.nextInt(2) == 0) {
-                    canvas.drawRect(
+
+                    /* Creating and Adding Rectangles to use it later
+                    * and Clearing the list not add previous ones too */
+
+                    val rectF = RectF(
                         j.toFloat(),
                         i.toFloat(),
                         (j + grid).toFloat(),
-                        (i + grid).toFloat(),
+                        (i + grid).toFloat()
+                    )
+
+                    rectFloatList.add(rectF)
+
+                    canvas.drawRect(
+                        rectF,
                         paint
                     )
                 }
             }
         }
 
+        /* */
         toDrawGrid = false
         hasGenerated = true
 
@@ -126,29 +170,11 @@ class CustomView @JvmOverloads constructor(
             interpolator = LinearInterpolator()
             addUpdateListener { valueAnimator ->
                 currentValue = valueAnimator.animatedValue as Int
-                Log.d("ANIMATOR", currentValue.toString())
+                Log.d("ANIMATOR", "currentValue: $currentValue")
                 invalidate()
             }
         }
         animator?.start()
-    }
-
-    private val rect = RectF(0f, 0f, 0f, 0f)
-
-    private var startX = 0f
-    private var startY = 0f
-    private var stopX = 0f
-    private var stopY = 0f
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        rect.set(0f, 0f, w.toFloat(), h.toFloat())
-        Log.e("CANVAS", "onSizeChanged called")
-
-        startX = 0f
-        startY= rect.height()
-        stopX = rect.width()
-        stopY = rect.height()
     }
 
     private fun drawRain(canvas: Canvas) {
@@ -160,27 +186,29 @@ class CustomView @JvmOverloads constructor(
     }
 
 
-
     private fun drawFlood(canvas: Canvas) {
 
         Log.d("CANVAS", "drawFlood called")
+
+        paint.color = Color.BLACK
+        for (rect in rectFloatList) {
+            canvas.drawRect(rect, paint)
+        }
 
         paint.apply {
             color = Color.BLUE
             style = Paint.Style.FILL
         }
 
-        Log.d("CANVAS", "h: ${rect.height()} / w: ${rect.width()}")
+        Log.d("CANVAS", "h: ${sizeRect.height()} / w: ${sizeRect.width()}")
 
-        canvas.drawRect(startX,startY, stopX, stopY, paint)
+        canvas.drawRect(startX, startY, stopX, stopY, paint)
 
         startY -= 5
 
-        Log.d("CANVAS", startY.toString())
+        Log.d("CANVAS", "drawFlood startY: $startY")
 
     }
-
-
 
 
 }
